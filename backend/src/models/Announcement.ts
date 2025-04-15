54e56920d2a473daf25e95837c4a2a7094a7e6f5
nier <patrykfulara7@gmail.com>
2025-04-15 14:17:42 +0200

    add annoucement repository

---
+++ backend/src/models/Announcement.ts
@@ -0,0 +1,9 @@
+export class Annoucement {
+    constructor(
+        public id: string,
+        public userId: string,
+        public title: string,
+        public content: string,
+        public created_at: string
+    ) {}
+}
